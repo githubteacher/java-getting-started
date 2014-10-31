3d7b7f37d8b0ac9307a8bbcc66a7891df041f6b7
GitHub Teacher <trainingdemos+githubteacher@github.com>
2014-10-31 09:28:43 -0600

    Blue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main extends HttpServlet {
 
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Auto Push - 9:45 AM!");
+    resp.getWriter().print("Hello from BLUE Auto Push - 9:45 AM!");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
